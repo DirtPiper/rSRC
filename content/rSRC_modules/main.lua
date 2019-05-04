cf98998c6aa6c5d30c7d33462e79b777226c5b6e
DirtPiper <oldrobloxguy@gmail.com>
2019-05-04 16:58:21 -0400

    IT WORKS
    FINALLYYYYY

--- content/rSRC_modules/main.lua
+++ content/rSRC_modules/main.lua
@@ -9,11 +9,19 @@ rSRC_modules = game:GetObjects("rbxasset://rSRC_modules/modules.rbxm")
 rSRC_modules[1].Parent = Workspace
 rSRC_main = game:GetObjects("rbxasset://rSRC_modules/rSRC_merged.rbxm")
 rSRC = rSRC_main[1]
-rSRC.Parent = Workspace
+rSRC.Parent = nil
 player = game.Players:CreateLocalPlayer(0)
 player:LoadCharacter()
 rserv = game:GetService("RunService")
 rserv:Run()
+fixed = false
+count = 0
 
+function counter(int)
+	if int == 5 then
+		rSRC.Parent = Workspace
+	end
+end
 
 Workspace.CurrentCamera.CameraSubject = player.Character.Humanoid
+rserv.Stepped:connect(function(a) counter(count) count = count + 1 end)
