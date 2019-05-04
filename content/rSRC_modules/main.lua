9e644c5a484a691b93fba7e050a13e9395964dde
DirtPiper <oldrobloxguy@gmail.com>
2019-05-04 13:57:49 -0400

    some more fixes
    about 80% working now, just need to make the camera change subject to the character and make the rSRC script actually execute

--- content/rSRC_modules/main.lua
+++ content/rSRC_modules/main.lua
@@ -5,16 +5,15 @@ playbin[3].Parent = game.StarterPack
 playbin[4].Parent = game.StarterPack
 playbin[5].Parent = game.StarterPack
 playbin[6].Parent = game.StarterPack
-raycast = game:GetObjects("rbxasset://rSRC_modules/raycasting.rbxm")
-raycast[1].Parent = Workspace
 rSRC_modules = game:GetObjects("rbxasset://rSRC_modules/modules.rbxm")
 rSRC_modules[1].Parent = Workspace
-rSRC_main = game:GetObjects("rbxasset://rSRC_modules/rSRC.rbxm")
+rSRC_main = game:GetObjects("rbxasset://rSRC_modules/rSRC_merged.rbxm")
 rSRC_main[1].Parent = Workspace
-
-rserv = game:GetService("RunService")
-rserv:Run()
 player = game.Players:CreateLocalPlayer(0)
 player:LoadCharacter()
+rserv = game:GetService("RunService")
+rserv:Run()
+--Workspace.Camera.CameraSubject = player.Humanoid
+
 
 
