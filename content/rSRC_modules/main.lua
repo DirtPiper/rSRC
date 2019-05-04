4f105c029de1a71b85d2a4c9587a6c75fc82b693
DirtPiper <oldrobloxguy@gmail.com>
2019-05-04 13:11:24 -0400

    hmmmmmmm
    raycasting doesn't want to actually work

--- content/rSRC_modules/main.lua
+++ content/rSRC_modules/main.lua
@@ -7,9 +7,13 @@ playbin[5].Parent = game.StarterPack
 playbin[6].Parent = game.StarterPack
 raycast = game:GetObjects("rbxasset://rSRC_modules/raycasting.rbxm")
 raycast[1].Parent = Workspace
+rSRC_modules = game:GetObjects("rbxasset://rSRC_modules/modules.rbxm")
+rSRC_modules[1].Parent = Workspace
 rSRC_main = game:GetObjects("rbxasset://rSRC_modules/rSRC.rbxm")
 rSRC_main[1].Parent = Workspace
 
+rserv = game:GetService("RunService")
+rserv:Run()
 game.Players:CreateLocalPlayer(0)
 game.Players.Player:LoadCharacter()
 
--- launch.bat
+++ launch.bat
@@ -1,1 +1,1 @@
-RobloxApp.exe -script print("dofile('rbxasset://rSRC_modules/main.lua')")
+RobloxApp.exe -script print("dofile('rbxasset://rSRC_modules/main.lua')") -script print("game:Load('rbxasset://rSRC_maps/t1.rbxl')") 
