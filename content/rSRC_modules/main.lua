d26bfa446f9f6177733f48c00e3d083e972a1b92
DirtPiper <oldrobloxguy@gmail.com>
2019-05-04 12:54:58 -0400

    It works!
    yeeeeeee

--- content/rSRC_modules/main.lua
+++ content/rSRC_modules/main.lua
@@ -1,4 +1,14 @@
-dofile("rbxasset://rSRC_modules/test.lua")
+playbin = game:GetObjects("rbxasset://rSRC_modules/playbin.rbxm")
+playbin[1].Parent = game.StarterPack
+playbin[2].Parent = game.StarterPack
+playbin[3].Parent = game.StarterPack
+playbin[4].Parent = game.StarterPack
+playbin[5].Parent = game.StarterPack
+playbin[6].Parent = game.StarterPack
+raycast = game:GetObjects("rbxasset://rSRC_modules/raycasting.rbxm")
+raycast[1].Parent = Workspace
+rSRC_main = game:GetObjects("rbxasset://rSRC_modules/rSRC.rbxm")
+rSRC_main[1].Parent = Workspace
 
 game.Players:CreateLocalPlayer(0)
 game.Players.Player:LoadCharacter()
