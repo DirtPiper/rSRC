b2dd622e82582474908049d88bd31ba16e68a324
DirtPiper <oldrobloxguy@gmail.com>
2019-05-05 22:47:06 -0400

    made exit and mkmap better

--- launch.bat
+++ launch.bat
@@ -35,7 +35,7 @@ GOTO begin
 )
 
 IF "%input%"=="exit" (
-EXIT
+GOTO end
 )
 
 ECHO Unknown command!
@@ -46,26 +46,23 @@ ECHO Starting rSRC...
 RobloxApp.exe -script print("dofile('rbxasset://rSRC_modules/main.lua')") -script print("game:Load('rbxasset://rSRC_maps/%input:~4%.rbxl')") 
 GOTO begin
 
-
 :listmaps
 for %%a in ("content\rSRC_maps\*") do @echo %%~na
 GOTO begin
 
 :mkmap
 ECHO Making map...
-ECHO ^<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation=" http://www.roblox.com/roblox.xsd" version="4"^> ^</roblox^> >>content/rSRC_maps/%input:~6%.rbxl
+ECHO ^<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation=" http://www.roblox.com/roblox.xsd" version="4"^> ^</roblox^> >content/rSRC_maps/%input:~6%.rbxl
 ECHO Made %input:~5%.rbxl!
 GOTO begin
-
 
 :edit
 ECHO Starting rSRC editor...
 RobloxApp.exe content/rSRC_maps/%input:~5%.rbxl
 GOTO begin
 
-
-
-
 :connect
 ECHO Sorry, networking hasn't been done yet!
 GOTO begin
+
+:end
