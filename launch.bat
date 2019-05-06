0b6f94e3a3d21ed8003b20a202fc7a3b5862b906
DirtPiper <oldrobloxguy@gmail.com>
2019-05-05 22:32:00 -0400

    rSRC terminal and metamod
    the terminal can be used to start rSRC, make new maps, edit existing maps, and eventually conenct to servers

--- launch.bat
+++ launch.bat
@@ -1,7 +1,62 @@
 @ECHO OFF
 
-set /p map="map "
+:begin
+set /p input="rSRC> "
 
+for %%i in (%input%) do (
+  IF "%%i"=="map" ( GOTO map )
+  IF "%%i"=="mkmap" ( GOTO mkmap )
+  IF "%%i"=="edit" ( GOTO edit )
+  IF "%%i"=="connect" ( GOTO connect )
+)
+
+IF "%input%"=="help" (
+ECHO Welcome to the rSRC terminal! Here's some commands to get you started:
+ECHO.
+ECHO help			displays this information
+ECHO.
+ECHO map mapname		starts rSRC and automatically loads the specified map
+ECHO.
+ECHO mkmap mapname		creates a new, empty map with the specified name
+ECHO.
+ECHO edit mapname		opens a map specifically for editing and automatically 
+ECHO 			loads the rSRC developer toolkit
+ECHO.
+ECHO connect ip port		connects to an rSRC server being hosted at 
+ECHO 			that address on that port 
+ECHO.
+ECHO.
+GOTO begin
+)
+
+IF "%input%"=="exit" (
+EXIT
+)
+
+ECHO Unknown command!
+GOTO begin
+
+:map
 ECHO Starting rSRC...
+RobloxApp.exe -script print("dofile('rbxasset://rSRC_modules/main.lua')") -script print("game:Load('rbxasset://rSRC_maps/%input:~4%.rbxl')") 
+GOTO begin
+)
 
-RobloxApp.exe -script print("dofile('rbxasset://rSRC_modules/main.lua')") -script print("game:Load('rbxasset://rSRC_maps/%map%.rbxl')") 
+:mkmap
+ECHO Making map...
+ECHO ^<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation=" http://www.roblox.com/roblox.xsd" version="4"^> ^</roblox^> >>content/rSRC_maps/%input:~6%.rbxl
+ECHO Made %input:~5%.rbxl!
+GOTO begin
+
+
+:edit
+ECHO Starting rSRC editor...
+RobloxApp.exe content/rSRC_maps/%input:~5%.rbxl
+GOTO begin
+
+
+
+
+:connect
+ECHO Sorry, networking hasn't been done yet!
+GOTO begin
